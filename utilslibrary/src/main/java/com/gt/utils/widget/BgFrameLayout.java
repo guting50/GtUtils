48dcc964c699a60a6704dd5bd47400d69514e501
GuTing <goodguting@gmail.com>
2020-05-03 13:23:36 +0800

    优化BgFrameLayout

--- utilslibrary/src/main/java/com/gt/utils/widget/BgFrameLayout.java
+++ utilslibrary/src/main/java/com/gt/utils/widget/BgFrameLayout.java
@@ -41,8 +41,8 @@ import androidx.annotation.RequiresApi;
  * <enum name="diagonal" value="-1" />
  * </attr>
  * <attr name="stroke_width" format="dimension" /><!--边框宽度-->
- * <attr name="stroke_dashGap" format="dimension" /><!--间隔-->
- * <attr name="stroke_dashWidth" format="dimension" /><!--点的大小-->
+ * <attr name="stroke_dash_gap" format="dimension" /><!--间隔-->
+ * <attr name="stroke_dash_width" format="dimension" /><!--点的大小-->
  * <attr name="corners_radius" format="dimension" /><!--圆角弧度-->
  * <attr name="corners_radius_left_top" format="dimension" /><!--圆角弧度 左上角-->
  * <attr name="corners_radius_right_top" format="dimension" /><!--圆角弧度 右上角-->
@@ -60,8 +60,8 @@ public class BgFrameLayout extends FrameLayout {
     private int stroke_end_color;
     private int stroke_gradual_change_orientation;
     private float stroke_width;
-    private float stroke_dashGap;
-    private float stroke_dashWidth;
+    private float stroke_dash_gap;
+    private float stroke_dash_width;
     private float corners_radius;
     private float corners_radius_left_top;
     private float corners_radius_right_top;
@@ -87,9 +87,9 @@ public class BgFrameLayout extends FrameLayout {
         stroke_start_color = typedArray.getColor(R.styleable.BgFrameLayout_stroke_start_color, stroke_color);
         stroke_end_color = typedArray.getColor(R.styleable.BgFrameLayout_stroke_end_color, stroke_color);
         stroke_gradual_change_orientation = typedArray.getInteger(R.styleable.BgFrameLayout_stroke_gradual_change_orientation, -1);
-        stroke_width = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_width, 0);
-        stroke_dashGap = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_dash_gap, 0);
-        stroke_dashWidth = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_dash_width, 0);
+        stroke_width = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_width, 0.0f);
+        stroke_dash_gap = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_dash_gap, 0);
+        stroke_dash_width = typedArray.getDimension(R.styleable.BgFrameLayout_stroke_dash_width, 0);
         corners_radius = typedArray.getDimension(R.styleable.BgFrameLayout_corners_radius, 0);
         corners_radius_left_top = typedArray.getDimension(R.styleable.BgFrameLayout_corners_radius_left_top, corners_radius);
         corners_radius_right_top = typedArray.getDimension(R.styleable.BgFrameLayout_corners_radius_right_top, corners_radius);
@@ -105,11 +105,12 @@ public class BgFrameLayout extends FrameLayout {
         paint.setAntiAlias(true);
         paint.setDither(true);
         Path path = new Path();
+        float strokeWidth = stroke_width * 2;
 
         //画边框
-        if (stroke_width != 0) {
+        {
             paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(stroke_width);
+            paint.setStrokeWidth(strokeWidth);
             int startX = 0, startY = 0, endX = this.getWidth(), endY = this.getHeight();
             if (stroke_gradual_change_orientation == 0) {
                 startY = endY = this.getHeight() / 2;
@@ -119,8 +120,9 @@ public class BgFrameLayout extends FrameLayout {
             Shader mShader = new LinearGradient(startX, startY, endX, endY, new int[]{stroke_start_color, stroke_end_color}, null, Shader.TileMode.CLAMP);
             paint.setShader(mShader);
 
-            paint.setPathEffect(new DashPathEffect(new float[]{stroke_dashWidth, stroke_dashGap}, 0));
-            path.addRoundRect(new RectF(stroke_width, stroke_width, this.getWidth() - stroke_width, this.getHeight() - stroke_width),
+            if (stroke_dash_width > 0)
+                paint.setPathEffect(new DashPathEffect(new float[]{stroke_dash_width, stroke_dash_gap}, 0));
+            path.addRoundRect(new RectF(strokeWidth / 2, strokeWidth / 2, this.getWidth() - strokeWidth / 2, this.getHeight() - strokeWidth / 2),
                     new float[]{corners_radius_left_top, corners_radius_left_top, corners_radius_right_top, corners_radius_right_top,
                             corners_radius_right_bottom, corners_radius_right_bottom, corners_radius_left_bottom, corners_radius_left_bottom},
                     Path.Direction.CW);
@@ -128,27 +130,29 @@ public class BgFrameLayout extends FrameLayout {
         }
 
         //画背景
-        paint.setStyle(Paint.Style.FILL);
-        //新建一个线性渐变，前两个参数是渐变开始的点坐标，第三四个参数是渐变结束的点的坐标。连接这2个点就拉出一条渐变线了，玩过PS的都懂。
-        // 然后那个数组是渐变的颜色。
-        // 下一个参数是渐变颜色的分布，如果为空，每个颜色就是均匀分布的。最后是模式，这里设置的是循环渐变
-        int startX = 0, startY = 0, endX = this.getWidth(), endY = this.getHeight();
-        if (solid_gradual_change_orientation == 0) {
-            startY = endY = this.getHeight() / 2;
-        } else if (solid_gradual_change_orientation == 1) {
-            startX = endX = this.getWidth() / 2;
-        }
-        Shader mShader = new LinearGradient(startX, startY, endX, endY, new int[]{solid_start_color, solid_end_color}, null, Shader.TileMode.CLAMP);
-        paint.setShader(mShader);
+        {
+            paint.setStyle(Paint.Style.FILL);
+            //新建一个线性渐变，前两个参数是渐变开始的点坐标，第三四个参数是渐变结束的点的坐标。连接这2个点就拉出一条渐变线了，玩过PS的都懂。
+            // 然后那个数组是渐变的颜色。
+            // 下一个参数是渐变颜色的分布，如果为空，每个颜色就是均匀分布的。最后是模式，这里设置的是循环渐变
+            int startX = 0, startY = 0, endX = this.getWidth(), endY = this.getHeight();
+            if (solid_gradual_change_orientation == 0) {
+                startY = endY = this.getHeight() / 2;
+            } else if (solid_gradual_change_orientation == 1) {
+                startX = endX = this.getWidth() / 2;
+            }
+            Shader mShader = new LinearGradient(startX, startY, endX, endY, new int[]{solid_start_color, solid_end_color}, null, Shader.TileMode.CLAMP);
+            paint.setShader(mShader);
 
-        //该方法5.0以上才支持
-//        path.addRoundRect(stroke_width * 2, stroke_width * 2, this.getWidth() - stroke_width * 2, this.getHeight() - stroke_width * 2,
+            //该方法5.0以上才支持
+//        path.addRoundRect(strokeWidth * 2, strokeWidth * 2, this.getWidth() - strokeWidth * 2, this.getHeight() - strokeWidth * 2,
 //                corners_radius, corners_radius, Path.Direction.CW);
-        path.addRoundRect(new RectF(stroke_width * 2, stroke_width * 2, this.getWidth() - stroke_width * 2, this.getHeight() - stroke_width * 2),
-                new float[]{corners_radius_left_top, corners_radius_left_top, corners_radius_right_top, corners_radius_right_top,
-                        corners_radius_right_bottom, corners_radius_right_bottom, corners_radius_left_bottom, corners_radius_left_bottom},
-                Path.Direction.CW);
-        canvas.drawPath(path, paint);
+            path.addRoundRect(new RectF(strokeWidth, strokeWidth, this.getWidth() - strokeWidth, this.getHeight() - strokeWidth),
+                    new float[]{corners_radius_left_top, corners_radius_left_top, corners_radius_right_top, corners_radius_right_top,
+                            corners_radius_right_bottom, corners_radius_right_bottom, corners_radius_left_bottom, corners_radius_left_bottom},
+                    Path.Direction.CW);
+            canvas.drawPath(path, paint);
+        }
         super.dispatchDraw(canvas);
     }
 
@@ -190,12 +194,12 @@ public class BgFrameLayout extends FrameLayout {
     }
 
     public void setStrokeDashGap(float stroke_dashGap) {
-        this.stroke_dashGap = stroke_dashGap;
+        this.stroke_dash_gap = stroke_dashGap;
         invalidate();
     }
 
     public void setStrokeDashWidth(float stroke_dashWidth) {
-        this.stroke_dashWidth = stroke_dashWidth;
+        this.stroke_dash_width = stroke_dashWidth;
         invalidate();
     }
 
