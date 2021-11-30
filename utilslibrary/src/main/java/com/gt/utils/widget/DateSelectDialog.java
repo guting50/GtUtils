11d09cb68bc47ea6cf4b1aba914aa337714adb1e
GUTING\guting <goodguting@gmail.com>
2021-11-30 15:38:53 +0800

    no message

--- utilslibrary/src/main/java/com/gt/utils/widget/DateSelectDialog.java
+++ utilslibrary/src/main/java/com/gt/utils/widget/DateSelectDialog.java
@@ -26,21 +26,14 @@ import java.util.TimerTask;
 import androidx.annotation.NonNull;
 
 public class DateSelectDialog extends Dialog {
-    /*初始显示的年份*/
-    private int defaultYear;
-    private int defaultMonth;
-    private int defaultDay;
-    private int defaultHour;
-    private int defaultMinute;
-
     private int currentYear;
     private int currentMonth;
     private int currentDay;
     private int currentHour;
     private int currentMinute;
 
-    /*结束的年份*/
-    private int endYear;
+    private Calendar startCalendar;
+    private Calendar endCalendar;
 
     TextView tv_cancel;
     TextView tv_sure;
@@ -56,11 +49,10 @@ public class DateSelectDialog extends Dialog {
     private ArrayWheelAdapter hourAdapter;
     private ArrayWheelAdapter minuteAdapter;
 
-    private OnSelectedListener onSelectedListener;
+    private DateSelectDialog.OnSelectedListener onSelectedListener;
 
     private String datePattern = "yyyy-MM-dd HH:mm";
     private Date defaultDate;
-    private boolean showGoOver = false;
 
     public DateSelectDialog(@NonNull Context context) {
         super(context, R.style.DateSelectDialog);
@@ -96,23 +88,23 @@ public class DateSelectDialog extends Dialog {
         if (defaultDate != null) {
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(defaultDate);
-            defaultYear = calendar.get(Calendar.YEAR);
-            defaultMonth = calendar.get(Calendar.MONTH);
-            defaultDay = calendar.get(Calendar.DAY_OF_MONTH);
-            defaultHour = calendar.get(Calendar.HOUR_OF_DAY);
-            defaultMinute = calendar.get(Calendar.MINUTE);
-
-            yearAdapter.setItems(getYearList());
-            loop_year.setCurrentItem(yearAdapter.indexOf(defaultYear + "年"));
-            monthAdapter.setItems(getMonthList());
-            loop_month.setCurrentItem(monthAdapter.indexOf(defaultMonth + 1 + "月"));
-            dayAdapter.setItems(getDayList());
-            loop_day.setCurrentItem(dayAdapter.indexOf(defaultDay + "日"));
-            hourAdapter.setItems(getHourList());
-            loop_hour.setCurrentItem(hourAdapter.indexOf(defaultHour + "时"));
-            minuteAdapter.setItems(getMinuteList());
-            loop_minute.setCurrentItem(minuteAdapter.indexOf(defaultMinute + "分"));
+            currentYear = calendar.get(Calendar.YEAR);
+            currentMonth = calendar.get(Calendar.MONTH);
+            currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+            currentMinute = calendar.get(Calendar.MINUTE);
         }
+
+        yearAdapter.setItems(getYearList());
+        loop_year.setCurrentItem(yearAdapter.indexOf(currentYear + "年"));
+        monthAdapter.setItems(getMonthList());
+        loop_month.setCurrentItem(monthAdapter.indexOf(currentMonth + 1 + "月"));
+        dayAdapter.setItems(getDayList());
+        loop_day.setCurrentItem(dayAdapter.indexOf(currentDay + "日"));
+        hourAdapter.setItems(getHourList());
+        loop_hour.setCurrentItem(hourAdapter.indexOf(currentHour + "时"));
+        minuteAdapter.setItems(getMinuteList());
+        loop_minute.setCurrentItem(minuteAdapter.indexOf(currentMinute + "分"));
     }
 
     public void setView() {
@@ -143,37 +135,93 @@ public class DateSelectDialog extends Dialog {
         return this;
     }
 
-    public DateSelectDialog setDefauleDate(Date date) {
-        defaultDate = date;
+    public DateSelectDialog setDefaultDate(Date date) {
+        this.defaultDate = date;
         return this;
+    }
+
+    public DateSelectDialog setDefaultDate(String dateStr) {
+        return setDefaultDate(dateStr, datePattern);
+    }
+
+    public DateSelectDialog setDefaultDate(String dateStr, String pattern) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(pattern).parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return setDefaultDate(date);
+    }
+
+    public DateSelectDialog setStartDate(Date date) {
+        if (startCalendar == null) {
+            startCalendar = Calendar.getInstance();
+        }
+        startCalendar.setTime(date);
+        return this;
+    }
+
+    public DateSelectDialog setStartDate(String dateStr) {
+        return setStartDate(dateStr, datePattern);
+    }
+
+    public DateSelectDialog setStartDate(String dateStr, String pattern) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(pattern).parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return setStartDate(date);
+    }
+
+    public DateSelectDialog setEndDate(Date date) {
+        if (endCalendar == null) {
+            endCalendar = Calendar.getInstance();
+        }
+        endCalendar.setTime(date);
+        return this;
+    }
+
+    public DateSelectDialog setEndDate(String dateStr) {
+        return setEndDate(dateStr, datePattern);
+    }
+
+    public DateSelectDialog setEndDate(String dateStr, String pattern) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(pattern).parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return setEndDate(date);
     }
 
     private void initTimeData() {
         Calendar calendar = Calendar.getInstance();
-        if (showGoOver) {
-            SimpleDateFormat formatter = new SimpleDateFormat(datePattern);
-            try {
-                calendar.setTime(formatter.parse("1970-01-01 00:00"));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+        currentYear = calendar.get(Calendar.YEAR);
+        currentMonth = calendar.get(Calendar.MONTH);
+        currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        currentMinute = calendar.get(Calendar.MINUTE);
+
+        if (endCalendar == null) {
+            endCalendar = Calendar.getInstance();
+            endCalendar.set(Calendar.YEAR, currentYear + 5);
         }
-        defaultYear = currentYear = calendar.get(Calendar.YEAR);
-        defaultMonth = currentMonth = calendar.get(Calendar.MONTH);
-        defaultDay = currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-        defaultHour = currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-        defaultMinute = currentMinute = calendar.get(Calendar.MINUTE);
-        endYear = defaultYear + 4;
+        if (startCalendar == null) {
+            startCalendar = Calendar.getInstance();
+            endCalendar.set(Calendar.YEAR, currentYear - 5);
+        }
     }
 
     private void initYmdPop() {
-        loop_year.setAdapter(yearAdapter = new ArrayWheelAdapter());
-        loop_month.setAdapter(monthAdapter = new ArrayWheelAdapter());
-        loop_day.setAdapter(dayAdapter = new ArrayWheelAdapter());
-        loop_hour.setAdapter(hourAdapter = new ArrayWheelAdapter());
-        loop_minute.setAdapter(minuteAdapter = new ArrayWheelAdapter());
-
-        updateYearList(0);
+        loop_year.setAdapter(yearAdapter = new DateSelectDialog.ArrayWheelAdapter());
+        loop_month.setAdapter(monthAdapter = new DateSelectDialog.ArrayWheelAdapter());
+        loop_day.setAdapter(dayAdapter = new DateSelectDialog.ArrayWheelAdapter());
+        loop_hour.setAdapter(hourAdapter = new DateSelectDialog.ArrayWheelAdapter());
+        loop_minute.setAdapter(minuteAdapter = new DateSelectDialog.ArrayWheelAdapter());
 
         loop_year.setOnItemSelectedListener(index -> {
             updateMonthList(index);
@@ -211,54 +259,46 @@ public class DateSelectDialog extends Dialog {
         tv_cancel.setOnClickListener(v -> dismiss());
     }
 
-    private void updateYearList(int index) {
+    private void updateYearList(int yearIndex) {
         yearAdapter.setItems(getYearList());
-        loop_year.setCurrentItem(index);
+        loop_year.setCurrentItem(yearIndex);
         updateMonthList(0);
     }
 
     private void updateMonthList(int yearIndex) {
-        defaultYear = currentYear + yearIndex;
+        String year = yearAdapter.getItems().get(yearIndex);
+        currentYear = Integer.parseInt(year.substring(0, year.length() - 1));
         monthAdapter.setItems(getMonthList());
         loop_month.setCurrentItem(0);
         updateDaysList(0);
     }
 
     private void updateDaysList(int monthIndex) {
-        if (defaultYear == currentYear) {
-            defaultMonth = currentMonth + monthIndex;
-        } else {
-            defaultMonth = monthIndex;
-        }
+        String month = monthAdapter.getItems().get(monthIndex);
+        currentMonth = Integer.parseInt(month.substring(0, month.length() - 1)) - 1;
         dayAdapter.setItems(getDayList());
         loop_day.setCurrentItem(0);
         updateHourList(0);
     }
 
-    private void updateHourList(int index) {
-        if (defaultMonth == currentMonth) {
-            defaultDay = currentDay + index;
-        } else {
-            defaultDay = index;
-        }
+    private void updateHourList(int dayIndex) {
+        String day = dayAdapter.getItems().get(dayIndex);
+        currentDay = Integer.parseInt(day.substring(0, day.length() - 1));
         hourAdapter.setItems(getHourList());
         loop_hour.setCurrentItem(0);
         updateMinuteList(0);
     }
 
-    private void updateMinuteList(int index) {
-        if (defaultDay == currentDay) {
-            defaultHour = currentHour + index;
-        } else {
-            defaultHour = index;
-        }
+    private void updateMinuteList(int hourIndex) {
+        String hour = hourAdapter.getItems().get(hourIndex);
+        currentHour = Integer.parseInt(hour.substring(0, hour.length() - 1));
         minuteAdapter.setItems(getMinuteList());
         loop_minute.setCurrentItem(0);
     }
 
     private ArrayList<String> getYearList() {
         ArrayList<String> yearList = new ArrayList<>();
-        for (int i = currentYear; i <= endYear; i++) {
+        for (int i = startCalendar.get(Calendar.YEAR); i <= endCalendar.get(Calendar.YEAR); i++) {
             yearList.add(i + "年");
         }
         return yearList;
@@ -266,11 +306,14 @@ public class DateSelectDialog extends Dialog {
 
     private ArrayList<String> getMonthList() {
         ArrayList<String> monthList = new ArrayList<>();
-        int i = 0;
-        if (defaultYear == currentYear) {
-            i = currentMonth;
+        int i = 0, totalMonths = 11;
+        if (currentYear == startCalendar.get(Calendar.YEAR)) {
+            i = startCalendar.get(Calendar.MONTH);
         }
-        for (; i <= 11; i++) {
+        if (currentYear == endCalendar.get(Calendar.YEAR)) {
+            totalMonths = endCalendar.get(Calendar.MONTH);
+        }
+        for (; i <= totalMonths; i++) {
             monthList.add(i + 1 + "月");
         }
         return monthList;
@@ -278,11 +321,14 @@ public class DateSelectDialog extends Dialog {
 
     private ArrayList<String> getDayList() {
         ArrayList<String> dayList = new ArrayList<>();
-        int i = 1;
-        int totalDays = getDays(isLeapYear(defaultYear), defaultMonth);
-        if (defaultYear == currentYear
-                && defaultMonth == currentMonth) {
-            i = currentDay;
+        int i = 1, totalDays = getDays(isLeapYear(currentYear), currentMonth);
+        if (currentYear == startCalendar.get(Calendar.YEAR) &&
+                currentMonth == startCalendar.get(Calendar.MONTH)) {
+            i = startCalendar.get(Calendar.DAY_OF_MONTH);
+        }
+        if (currentYear == endCalendar.get(Calendar.YEAR) &&
+                currentMonth == endCalendar.get(Calendar.MONTH)) {
+            totalDays = endCalendar.get(Calendar.DAY_OF_MONTH);
         }
         for (; i <= totalDays; i++) {
             dayList.add(i + "日");
@@ -292,13 +338,18 @@ public class DateSelectDialog extends Dialog {
 
     private ArrayList<String> getHourList() {
         ArrayList<String> hourList = new ArrayList<>();
-        int i = 0;
-        if (defaultYear == currentYear
-                && defaultMonth == currentMonth
-                && defaultDay == currentDay) {
-            i = currentHour;
+        int i = 0, totalHours = 23;
+        if (currentYear == startCalendar.get(Calendar.YEAR) &&
+                currentMonth == startCalendar.get(Calendar.MONTH) &&
+                currentDay == startCalendar.get(Calendar.DAY_OF_MONTH)) {
+            i = startCalendar.get(Calendar.HOUR_OF_DAY);
         }
-        for (; i <= 23; i++) {
+        if (currentYear == endCalendar.get(Calendar.YEAR) &&
+                currentMonth == endCalendar.get(Calendar.MONTH) &&
+                currentDay == endCalendar.get(Calendar.DAY_OF_MONTH)) {
+            totalHours = endCalendar.get(Calendar.HOUR_OF_DAY);
+        }
+        for (; i <= totalHours; i++) {
             hourList.add(i + "时");
         }
         return hourList;
@@ -306,14 +357,20 @@ public class DateSelectDialog extends Dialog {
 
     private ArrayList<String> getMinuteList() {
         ArrayList<String> minuteList = new ArrayList<>();
-        int i = 0;
-        if (defaultYear == currentYear
-                && defaultMonth == currentMonth
-                && defaultDay == currentDay
-                && defaultHour == currentHour) {
-            i = currentMinute;
+        int i = 0, totalMinutes = 59;
+        if (currentYear == startCalendar.get(Calendar.YEAR) &&
+                currentMonth == startCalendar.get(Calendar.MONTH) &&
+                currentDay == startCalendar.get(Calendar.DAY_OF_MONTH) &&
+                currentHour == startCalendar.get(Calendar.HOUR_OF_DAY)) {
+            i = startCalendar.get(Calendar.MINUTE);
         }
-        for (; i <= 59; i++) {
+        if (currentYear == endCalendar.get(Calendar.YEAR) &&
+                currentMonth == endCalendar.get(Calendar.MONTH) &&
+                currentDay == endCalendar.get(Calendar.DAY_OF_MONTH) &&
+                currentHour == endCalendar.get(Calendar.HOUR_OF_DAY)) {
+            totalMinutes = endCalendar.get(Calendar.MINUTE);
+        }
+        for (; i <= totalMinutes; i++) {
             minuteList.add(i + "分");
         }
         return minuteList;
@@ -440,7 +497,7 @@ public class DateSelectDialog extends Dialog {
         }
     }
 
-    public DateSelectDialog setOnSelectedListener(OnSelectedListener onSelectedListener) {
+    public DateSelectDialog setOnSelectedListener(DateSelectDialog.OnSelectedListener onSelectedListener) {
         this.onSelectedListener = onSelectedListener;
         return this;
     }
